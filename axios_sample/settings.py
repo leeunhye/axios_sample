ef9d232257ba9dfe2166c7392fb106cc2f6d94a6
leeunhye <lee.eunhye@planaria.co.jp>
2021-09-09 16:16:02 +0900

    restful framework通信

--- axios_sample/settings.py
+++ axios_sample/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'communicate'
 ]
 
@@ -120,4 +121,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, "web/static")
--- communicate/views/messageListView.py
+++ communicate/views/messageListView.py
@@ -9,9 +9,16 @@ class MessageListView(generics.ListAPIView):
     """
     def get(self, request, *args, **kwargs):
 
-        data = [
+        data = self.get_queryset()
+
+        return Response(data)
+
+    def get_queryset(self):
+        # DBからデータを取得します。
+        queryset = [
             {'company': '営業部', 'name': '山田太郎', 'apply_date': '2021-09-06', 'title': '申請タイトルになる', 'content': '申請内容なんちゃら'},
             {'company': '営業部', 'name': '佐藤次郎', 'apply_date': '2021-09-08', 'title': '給料の支給', 'content': 'お金ほしいです！'},
-            {'company': '広報部', 'name': '鈴木花子', 'apply_date': '2021-09-09', 'title': '新しいボールペン購入', 'content': '10色ペンセットがほしいかも'}
+            {'company': '広報部', 'name': '大阪花子', 'apply_date': '2021-09-09', 'title': '新しいボールペン購入', 'content': '10色ペンセットがほしいかも'}
         ]
-        return Response(data)
+
+        return queryset
